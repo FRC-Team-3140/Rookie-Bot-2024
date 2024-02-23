98ffdf4b300bf016800fc93a17cb5be76bc7bc83
Grace Preston <143225518+Grass-Rat@users.noreply.github.com>
2024-02-23 17:40:43 -0500

    edited climber code -gp

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,30 +4,111 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Climber;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+import com.revrobotics.CANSparkBase.IdleMode;
+// import frc.robot.libs.XboxCotroller;
+// import frc.robot.subsystems.SwerveDrive;
 
-  private RobotContainer m_robotContainer;
+public class Robot extends TimedRobot implements Constants{
+  // private Command m_autonomousCommand;
+  private RobotContainer m_robotContainer; 
+  //  public static CANSparkMax left = new CANSparkMax(9, MotorType.kBrushless);
+  //  public static CANSparkMax right = new CANSparkMax(8, MotorType.kBrushless);
+public Climber climber = new Climber();
+// public Climber right = new Climber();
+   NetworkTableInstance inst = NetworkTableInstance.getDefault();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  
+    NetworkTable motorInfo = inst.getTable("motorinfo");
+   
+   public XboxController x = new XboxController(0);
+  // private static XboxCotroller m_controller = RobotContainer.controller;
+  // private static SwerveDrive swerve = RobotContainer.swerve;
+
+  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
+  @Override
+  public void autonomousPeriodic() {
+    // driveWithJoystick(false);
+    // swerve.updateOdometry();
+  }
+  @Override
+  public void teleopInit() {
+    
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // driveWithJoystick(true);
+
+    //trmporary code
+    if(x.getAButton() || x.getBButton() || x.getXButton() || x.getYButton()){
+    
+      if(x.getAButton()){
+        climber.lowerLeft();
+      }
+
+      if(x.getBButton()){
+        climber.lowerRight();
+      }
+
+      if(x.getXButton()){
+        climber.raiseLeft();
+      }
+
+      if(x.getYButton()){
+        climber.raiseRight();
+      }
+    }else{
+      climber.stopLeft();
+      climber.stopRight();
+    }
+    
+
+
+    // motorInfo.getEntry("leftvoltage").setDouble(left.getBusVoltage()*left.getAppliedOutput());
+    // motorInfo.getEntry("leftcurrent").setDouble(left.getOutputCurrent());
+    // double leftvoltage = motorInfo.getEntry("leftvoltage").getDouble(0);
+    // double leftcurrent = motorInfo.getEntry("leftcurrent").getDouble(0);
+    // motorInfo.getEntry("leftpower").setDouble(leftcurrent*leftvoltage);
+
+    // motorInfo.getEntry("rightvoltage").setDouble(right.getBusVoltage()*right.getAppliedOutput());
+    // motorInfo.getEntry("rightcurrent").setDouble(right.getOutputCurrent());
+    // double rightvoltage = motorInfo.getEntry("rightvoltage").getDouble(0);
+    // double rightcurrent = motorInfo.getEntry("rightcurrent").getDouble(0);
+    // motorInfo.getEntry("rightpower").setDouble(rightcurrent*rightvoltage);
+    
+    
+  }
+
+  // Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+
   @Override
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    
+    motorInfo.getEntry("leftvoltage").setDefaultValue(0);
+    motorInfo.getEntry("leftcurrent").setDefaultValue(0);
+    motorInfo.getEntry("leftpower").setDefaultValue(0);
+
+    motorInfo.getEntry("rightvoltage").setDefaultValue(0);
+    motorInfo.getEntry("rightcurrent").setDefaultValue(0);
+    motorInfo.getEntry("rightpower").setDefaultValue(0);
   }
 
   /**
@@ -56,37 +137,21 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    // m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
+    // if (m_autonomousCommand != null) {
+    //   m_autonomousCommand.schedule();
+    // }
   }
 
   /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
 
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
-
+  
   @Override
   public void testInit() {
     // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
+    // CommandScheduler.getInstance().cancelAll();
   }
 
   /** This function is called periodically during test mode. */
@@ -100,4 +165,25 @@ public class Robot extends TimedRobot {
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
+
+
+
+  private void driveWithJoystick(boolean fieldRelative) {
+    // Get the x speed. We are inverting this because Xbox controllers return
+    // negative values when we push forward.
+    // final var xSpeed = -m_controller.getLeftY() * maxSpeed;
+
+    // Get the y speed or sideways/strafe speed. We are inverting this because
+    // we want a positive value when we pull to the left. Xbox controllers
+    // return positive values when you pull to the right by default.
+    // final var ySpeed = m_controller.getLeftX() * maxSpeed;
+
+    // Get the rate of angular rotation. We are inverting this because we want a
+    // positive value when we pull to the left (remember, CCW is positive in
+    // mathematics). Xbox controllers return positive values when you pull to
+    // the right by default.
+    // final var rot = -m_controller.getRightX() * maxChassisTurnSpeed;
+
+    // swerve.drive(xSpeed, ySpeed, rot, fieldRelative);
+  }
 }
